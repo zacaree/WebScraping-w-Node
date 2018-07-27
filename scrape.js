077a9e4a75c3f4ec40589f44ace09d1311ac7b1d
Zac <zacaree@gmail.com>
2018-07-27 08:34:11 -0500

    .

--- scrape.js
+++ scrape.js
@@ -4,21 +4,24 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
-
 var fs = require('fs');
 
 
+var gravelyUrl1 = 'https://www.gravely.com/en-us/stand-on-mowers/pro-stance'
+var gravelyUrl2 = 'https://www.gravely.com/en-us/zero-turn-mowers/pro-turn-400-xdz'
 
-// Now we'll load the front page of Hacker News with a simple request and display the HTML code of the page, if no error occurs and the HTTP status code equals 200.
-request('https://www.gravely.com/en-us/zero-turn-mowers/pro-turn-400-xdz', function (error, response, html) {
+var gravelyStructure = 'table.product-specs--spec-table'
+
+
+request(gravelyUrl1, function (error, response, html) {
   if (!error && response.statusCode == 200) {
     // console.log(html);
     var $ = cheerio.load(html);
-    $('table.product-specs--spec-table').each(function (i, element) {
+    $(gravelyStructure).each(function (i, element) {
       var a = $(this);
       fs.writeFileSync('log.txt', a, function (err) {
         if (err) {
-          console.log('append failed');
+          console.log('failed');
         } else {
           console.log('done');
         }
